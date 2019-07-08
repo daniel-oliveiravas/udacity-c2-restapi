f09a78bb932e725521f5c9c193c064354adcb226
Daniel Vasconcelos <daniel.oliveiravas@gmail.com>
2019-07-08 11:52:02 -0300

    Change configs to use environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,10 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_reigion": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
+  "username": process.env.DATABASE_USERNAME,
+  "password": process.env.DATABASE_PASSWORD,
+  "database": process.env.DATABASE_NAME,
+  "host": process.env.DATABASE_HOST,
+  "dialect": process.env.DATABASE_DIALECT,
+  "aws_region": process.env.AWS_REGION,
+  "aws_profile": process.env.AWS_PROFILE,
+  "aws_media_bucket": process.env.MEDIA_BUCKET
 }
